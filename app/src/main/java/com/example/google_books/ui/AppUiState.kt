cb1817a3d99383a51f2fff1f7e01acbd27e304ad
Kirill Zverev <kirill.zverev@whitetigersoft.ru>
2025-04-08 10:40:13 +0300

    fix: misstype name variable

--- app/src/main/java/com/example/google_books/ui/AppUiState.kt
+++ app/src/main/java/com/example/google_books/ui/AppUiState.kt
@@ -5,7 +5,7 @@ import com.example.google_books.model.Volume
 data class AppUiState(
     val selectedVolume: Volume? = null,
     var booksListScreenState: ScreenState = ScreenState.Loading,
-    var bookDetails: ScreenState = ScreenState.Loading,
+    var bookDetailsScreenState: ScreenState = ScreenState.Loading,
 )
 
 
