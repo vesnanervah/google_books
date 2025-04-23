2b65556050949cdd8fc3018b21a67731de560310
Kirill Zverev <vesnanervakh@gmail.com>
2025-04-23 12:19:48 +0300

    feat: change loading indicator in screen state resolver

--- app/src/main/java/com/example/google_books/ui/common/ScreenStateResolverWidget.kt
+++ app/src/main/java/com/example/google_books/ui/common/ScreenStateResolverWidget.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -24,11 +27,8 @@ fun ScreenStateResolverWidget(screenState: ScreenState, onRetryAction: () -> Uni
     }
 }
 
-// TODO: better loader view
 @Composable
 private fun ScreenStateLoadingIndicator() {
-    Box(Modifier.background(Color.Black.copy(0.2F)).padding(8.dp)) {
-        Image(painterResource(R.drawable.loading_img), "Loading", Modifier.size(160.dp))
-    }
+    CircularProgressIndicator(Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
 }
 
