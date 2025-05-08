8bdce9ca5cc8a1da5b72fed256578af7cb9b7265
Kirill Zverev <vesnanervakh@gmail.com>
2025-05-08 21:12:36 +0300

    temporary fix: replace http images links with https

--- app/src/main/java/com/example/google_books/model/Volume.kt
+++ app/src/main/java/com/example/google_books/model/Volume.kt
@@ -1,5 +1,7 @@
 package com.example.google_books.model
 
+import android.util.Log
+import android.webkit.URLUtil
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,4 +9,31 @@ data class Volume(
     val id: String,
     val volumeInfo: VolumeInfo,
     val saleInfo: VolumeSaleInfo? = null,
-)
+) {
+
+    init {
+        replaceHttpImagesLinks(this)
+    }
+
+    companion object {
+        private fun replaceHttpImagesLinks(volume: Volume) {
+            // TODO: iterate through class fields with reflection
+            val links = volume.volumeInfo.imageLinks
+            links.thumbnail = replaceHttpLink(links.thumbnail)!!
+            links.small = replaceHttpLink(links.small)
+            links.medium = replaceHttpLink(links.medium)
+            links.large = replaceHttpLink(links.large)
+
+        }
+
+        // OkHttp client in coil doesn't like http links, fetched from books api
+        private fun replaceHttpLink(url: String?): String? {
+            if (url == null || URLUtil.isHttpsUrl(url)) {
+                return url
+            }
+            val result = url.split(Regex("^http")).joinToString("s")
+            Log.d("http_convert", result)
+            return url.split(Regex("^http")).joinToString("https")
+        }
+    }
+}
--- app/src/main/java/com/example/google_books/model/VolumeImageLinks.kt
+++ app/src/main/java/com/example/google_books/model/VolumeImageLinks.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class VolumeImageLinks(
-    val thumbnail: String,
-    val small: String? = null,
-    val medium: String? = null,
-    val large: String? = null,
+    var thumbnail: String,
+    var small: String? = null,
+    var medium: String? = null,
+    var large: String? = null,
 )
