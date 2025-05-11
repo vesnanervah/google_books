fde3e33686d340968c20eb302893024bbb7306c2
Kirill Zverev <vesnanervakh@gmail.com>
2025-05-11 12:04:46 +0300

    feat: unfocus after search submit, replace callback in separate function, use the function in onDone keyboard cb

--- app/src/main/java/com/example/google_books/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/google_books/ui/screens/SearchScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,22 +16,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun SearchScreen(modifier: Modifier = Modifier, onSearchSubmit: (search: String) -> Unit) {
+    val focusManager = LocalFocusManager.current
     var value by remember { mutableStateOf("")  }
-    val focusRequester = remember { FocusRequester() }
     var showError by remember { mutableStateOf(false) }
 
     Column(modifier, Arrangement.Center, Alignment.CenterHorizontally) {
         TextField(
             value,
             { value = it },
-            Modifier.focusRequester(focusRequester),
             isError = showError,
             singleLine = true,
             placeholder = {
@@ -40,18 +42,27 @@ fun SearchScreen(modifier: Modifier = Modifier, onSearchSubmit: (search: String)
                 if (showError) {
                     Text("Try search something else")
                 }
-            }
+            },
+            keyboardActions = KeyboardActions(onDone = {
+                submit(value, focusManager, { showError = true }, onSearchSubmit)
+            }),
+
         )
         Spacer(Modifier.padding(8.dp))
         Button({
-            if (value.isEmpty()){
-                showError = true
-            } else {
-                onSearchSubmit(value)
-            }
+            submit(value, focusManager, { showError = true }, onSearchSubmit)
         })  {
             Text("Search")
         }
+    }
+}
+
+private fun submit( value: String, focusManager: FocusManager, onErrorOccured: () -> Unit, onSearchSubmit: (search: String) -> Unit) {
+    focusManager.clearFocus()
+    if (value.isEmpty()){
+        onErrorOccured()
+    } else {
+        onSearchSubmit(value)
     }
 }
 
--- readme.md
+++ readme.md
@@ -14,7 +14,6 @@ An application for easy books search via google books api
   3) Large layout
 
 ### Closest tasks:
-- Remove focus after search submit
 - Adaptive font size of search result list item title
 - Appbar should display user query string in search page and book title in details page
 - Partial loading in search results page
